93ce3826ddf01a8e9bc08285ce1a3298e8d71f13
Akitaka-dev <akitaka.runesh@gmail.com>
2024-08-05 12:38:58 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get " localhost:3000/ask", to: "ask#action"
+  get "/ask", to: "QuestionsController#ask"
 
   # Defines the root path route ("/")
   # root "posts#index"
